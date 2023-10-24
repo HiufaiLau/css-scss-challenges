76fbab8f2b7d69033cc11315072db7254999ecc0
HiufaiLau <hiufailau.41@gmail.com>
2023-10-24 17:10:06 +0200

    added active link behavior at scroll-menu

--- scroll-menu-like-airbnb/script.js
+++ scroll-menu-like-airbnb/script.js
@@ -20,3 +20,31 @@ menuItems.forEach((item) => {
     item.scrollIntoView({ behavior: "smooth", block: "center" });
   });
 });
+
+// click item
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const items = document.querySelectorAll(".item");
+
+//   items.forEach((item) => {
+//     item.addEventListener("click", function (event) {
+//       event.preventDefault();
+//       items.forEach((i) => i.classList.remove("active"));
+//       item.classList.add("active");
+//     });
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const items = document.querySelectorAll(".item");
+
+  items.forEach((item) => {
+    item.addEventListener("click", function (event) {
+      event.preventDefault();
+      items.forEach((i) => {
+        i.style.borderBottom = "none"; // Remove border from all items
+      });
+      item.style.borderBottom = "2px solid #0074d9"; // Add border to the clicked item
+    });
+  });
+});
